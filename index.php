1fef9cd222807b2e2cc5cbee781cba76f170f25d
vladteen <vladteen1@yandex.ru>
2019-11-04 20:28:09 +0300

    Подключили php скрипт к базе данных (БД), название и описание берётся из неё же!

--- index.php
+++ index.php
@@ -1,9 +1,41 @@
+<?php
+
+$link = mysqli_connect("localhost","site","etis","site");
+
+mysqli_set_charset($link, "utf8");
+
+if(mysqli_connect_errno())
+{
+	echo mysqli_connect_error;
+}
+$filmId=rand(1,3);
+$sqlQuery="select * from films where id=".$filmId;
+$sqlResult=mysqli_query($link, $sqlQuery);
+$filmRows=mysqli_fetch_all($sqlResult, MYSQLI_ASSOC);
+$filmRow=$filmRows [0];
+ 
+
+?>
+
 <!DOCTYPE html>
 <html lang="ru">
-   <head>
-    <meta charset="utf-8">
-    <title>КИНО</title>
-   </head>
-   <body>
-   </body>
+
+<head>
+	<meta charset="utf-8">
+	<title>КИНО</title>
+</head>
+
+<body>
+	<h1>
+		<?php
+			echo $filmRow["name"];
+		?>
+	</h1>
+	<h3>
+		<?php
+			echo $filmRow["description"];
+		?>
+	</h3>
+</body>
+
 </html>
